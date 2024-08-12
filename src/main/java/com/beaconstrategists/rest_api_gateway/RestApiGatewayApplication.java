d26a3906f7a7664e4804d3d18b9c80f272aa98f4
David Hustace <david@beaconstrategists.com>
2024-08-12 13:15:43 -0400

    Initial commit with Spring Boot reverse proxy with OAuth 2.0 support

---
+++ src/main/java/com/beaconstrategists/rest_api_gateway/RestApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.beaconstrategists.rest_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beaconstrategists/rest_api_gateway/RestApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beaconstrategists.rest_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
